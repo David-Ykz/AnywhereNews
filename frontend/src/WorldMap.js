b193f8427e353283446f071e92b3619e00eeccfb
David-Ykz <kzdavid314@gmail.com>
2024-01-20 17:46:43 -0500

    changed world map to english style

--- frontend/src/WorldMap.js
+++ frontend/src/WorldMap.js
@@ -1,14 +1,19 @@
 import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+//import L from 'Leaflet';
 
 
 function WorldMap() {
+//    var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+//        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+ //       maxZoom: 16
+  //  });
     const position = [51.505, -0.09]
     return (
-        <MapContainer center={position} zoom={13} scrollWheelZoom={true} style={{ height: '500px', width: '100%' }}>
+        <MapContainer center={position} zoom={13} maxZoom={5} scrollWheelZoom={true} style={{ height: '500px', width: '100%' }}>
             <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy;'
+                url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}.png"
             />
         </MapContainer>
     );
