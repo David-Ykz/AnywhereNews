764f297365de5b0662cbf50ce0f7e1e6b274dd55
David-Ykz <kzdavid314@gmail.com>
2024-01-25 21:46:26 -0500

    switched financial news to use cors

--- backend/app.js
+++ backend/app.js
@@ -33,34 +33,56 @@ app.post("/country-news", (request, response) => {
     };
 
     axios.request(options).then(function (apiResponse) {
-        console.log(apiResponse.data);
-        response.send(apiResponse.data);
+        console.log(apiResponse.data.articles);
+        response.send(apiResponse.data.articles);
     }).catch(function (error) {
         console.error(error);
     });
 })
 
 app.get("/financial-news", (request, response) => {
-    console.log("Financial News");
-    'use strict';
-    const apiRequest = require('request');
-    const baseUrl = "https://www.alphavantage.co/query?function=NEWS_SENTIMENT";
-    const url = baseUrl + "&apikey=" + process.env.ALPHA_VANTAGE_API_KEY;
-//    const url = "https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=demo";
-    apiRequest.get({
-        url: url,
-        json: true,
-        headers: {'User-Agent': 'request'}
-    }, (err, res, data) => {
-        if (err) {
-            console.log('Error:', err);
-        } else if (res.statusCode !== 200) {
-            console.log('Status:', res.statusCode);
-        } else {
-            console.log(data.feed);
-            response.send(data.feed);
+
+    var options = {
+        method: 'GET',
+        url: 'https://www.alphavantage.co/query?',
+        params: {
+            function: 'NEWS_SENTIMENT',
+            topics: 'finance',
+            apikey: process.env.ALPHA_VANTAGE_API_KEY
         }
+    };
+
+    axios.request(options).then(function (apiResponse) {
+        console.log(apiResponse.data);
+        console.log(apiResponse.data.feed);
+        response.send(apiResponse.data.feed);
+    }).catch(function (error) {
+        console.error(error);
     });
+
+
+
+    console.log("Financial News");
+//     'use strict';
+//     const apiRequest = require('request');
+//     const baseUrl = "https://www.alphavantage.co/query?function=NEWS_SENTIMENT";
+//     const url = baseUrl + "&apikey=" + process.env.ALPHA_VANTAGE_API_KEY;
+// //    const url = "https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=demo";
+//     apiRequest.get({
+//         url: 'https://www.alphavantage.co/query?',
+//         function: 'NEWS_SENTIMENT',
+//         topics: 'earnings,economy_fiscal,economy_monetary,economy_macro,finance',
+//         apikey: process.env.ALPHA_VANTAGE_API_KEY
+//     }, (err, res, data) => {
+//         if (err) {
+//             console.log('Error:', err);
+//         } else if (res.statusCode !== 200) {
+//             console.log('Status:', res.statusCode);
+//         } else {
+//             console.log(data.feed);
+//             response.send(data.feed);
+//         }
+//     });
 })
 
 
